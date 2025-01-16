c90bf65a329b431cb314b9e0c3faa75520410b60
Jose Galeana <kevingaleana@yahoo.com>
2025-01-15 23:03:42 -0600

    all done

---
+++ index.js
@@ -0,0 +1,16 @@
+let code1
+code1= 10
+code1= 7 + 3
+let code2
+code2= 40
+code2 = (100 - 20) / 2
+let code3
+code3= 39
+code3= 100 % 61
+let message
+message= "the vault is secured. the combination is:"
+let codeA
+codeA= code1 + "-" + code2 + "-" + code3
+let codeB
+codeB= `${code1}-${code2}-${code3}`
+
